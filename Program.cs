5d55ab775a347acbaed8405460e43b8edc2f57ad
Daniel Egger <dan.egg@outlook.com>
2023-10-14 17:00:06 +0200

    Add proper console formatting for IFD fields

--- Program.cs
+++ Program.cs
@@ -36,29 +36,35 @@ Offsets:
     TIFF (...) - Little Endian!
         Byte Order (2 bytes)
         Magic Number (2 bytes)
-        IFD Record Offset (4 bytes)
+        IFD Header Offset (4 bytes)
     
-    @IFD Record Offset:
+    @IFD Header Offset:
         Field Count (2 bytes)
         Fields:
             Field Tag (2 bytes)
             Field Type (2 bytes)
             Value Count (4 bytes)
             Value Offset (4 bytes)
-        IFD Record Offset (4 bytes)
+        IFD Header Offset (4 bytes)
 **/
 
+/// https://developer.adobe.com/content/dam/udp/en/open/standards/tiff/TIFF6.pdf
+/// https://developer.adobe.com/content/dam/udp/en/open/standards/tiff/TIFFPM6.pdf
 
-/// File:
-var file = File.Open(args[0], FileMode.Open, FileAccess.Read);
+// -------------------------------------------------------------------------- //
 
+/// RAF File:
 
+var RafFile = File.Open(args[0], FileMode.Open, FileAccess.Read);
+
+// -------------------------------------------------------------------------- //
 
 /// RAF Offsets:
+
 var RafHeader = new byte[24];
 
-file.Position = 84;
-file.Read(RafHeader);
+RafFile.Position = 84;
+RafFile.Read(RafHeader);
 
 var JpegImageOffset = BinaryPrimitives.ReadInt32BigEndian(RafHeader[0..]);
 var JpegImageLength = BinaryPrimitives.ReadInt32BigEndian(RafHeader[4..]);
@@ -67,77 +73,114 @@ var CfaHeaderLength = BinaryPrimitives.ReadInt32BigEndian(RafHeader[12..]);
 var CfaRecordOffset = BinaryPrimitives.ReadInt32BigEndian(RafHeader[16..]);
 var CfaRecordLength = BinaryPrimitives.ReadInt32BigEndian(RafHeader[20..]);
 
-Console.WriteLine("JPEG Image:\tOffset = {0:X};\tLength = {1:X}", JpegImageOffset, JpegImageLength);
-Console.WriteLine("CFA Header:\tOffset = {0:X};\tLength = {1:X}", CfaHeaderOffset, CfaHeaderLength);
-Console.WriteLine("CFA Record:\tOffset = {0:X};\tLength = {1:X}", CfaRecordOffset, CfaRecordLength);
+Console.WriteLine("JPEG Image:\tOffset = 0x{0:X};\tLength = 0x{1:X}", JpegImageOffset, JpegImageLength);
+Console.WriteLine("CFA Header:\tOffset = 0x{0:X};\tLength = 0x{1:X}", CfaHeaderOffset, CfaHeaderLength);
+Console.WriteLine("CFA Record:\tOffset = 0x{0:X};\tLength = 0x{1:X}", CfaRecordOffset, CfaRecordLength);
 
+Console.WriteLine("~~~");
 
+// -------------------------------------------------------------------------- //
 
 /// TIF Offsets:
+
 var TifHeader = new byte[8];
 
-file.Position = CfaRecordOffset;
-file.Read(TifHeader);
+RafFile.Position = CfaRecordOffset;
+RafFile.Read(TifHeader);
 
-var IfdRecordOffset = BinaryPrimitives.ReadInt32LittleEndian(TifHeader[4..]);
+var IfdHeaderOffset = BinaryPrimitives.ReadInt32LittleEndian(TifHeader[4..]);
 
-Console.WriteLine("IFD Record:\tOffset = {0:X}", IfdRecordOffset);
+Console.WriteLine("IFD Header:\tOffset = 0x{0:X}", IfdHeaderOffset);
 
+Console.WriteLine("~~~");
 
+// -------------------------------------------------------------------------- //
 
-/// IFD Root Header:
+/// IFD Header:
+
 var IfdHeader = new byte[2];
 
-file.Position = CfaRecordOffset + IfdRecordOffset;
-file.Read(IfdHeader);
+RafFile.Position = CfaRecordOffset + IfdHeaderOffset;
+RafFile.Read(IfdHeader);
 
-var IfdFieldCount = BinaryPrimitives.ReadInt16LittleEndian(IfdHeader[0..]);
+var IfdRecordCount = BinaryPrimitives.ReadInt16LittleEndian(IfdHeader[0..]);
 
-Console.WriteLine("IFD Record:\tFields = {0:X}", IfdFieldCount);
+Console.WriteLine("IFD Header:\tCount = {0}", IfdRecordCount);
 
+Console.WriteLine("~~~");
 
-/// IFD Root Fields:
-var IfdField = new byte[12];
+// -------------------------------------------------------------------------- //
 
-file.Position = CfaRecordOffset + IfdRecordOffset + 2;
-file.Read(IfdField);
+/// IFD Record:
 
-var tag = BinaryPrimitives.ReadInt16LittleEndian(IfdField[0..]);
-var type = BinaryPrimitives.ReadInt16LittleEndian(IfdField[2..]);
-var count = BinaryPrimitives.ReadInt32LittleEndian(IfdField[4..]);
-var value = BinaryPrimitives.ReadInt32LittleEndian(IfdField[8..]);
+var IfdRecord = new byte[12];
 
-Console.WriteLine("\nIFD Field:\tTag = {0:X};\tType = {1:X}", tag, type);
-Console.WriteLine("IFD Field:\tCount = {0};\tValue = {1}", count, value);
+RafFile.Position = CfaRecordOffset + IfdHeaderOffset + 2;
+RafFile.Read(IfdRecord);
 
+var ifdTag = BinaryPrimitives.ReadInt16LittleEndian(IfdRecord[0..]);
+var ifdType = BinaryPrimitives.ReadInt16LittleEndian(IfdRecord[2..]);
+var ifdCount = BinaryPrimitives.ReadInt32LittleEndian(IfdRecord[4..]);
+var ifdValue = BinaryPrimitives.ReadInt32LittleEndian(IfdRecord[8..]);
 
-/// https://www.adobe.io/content/dam/udp/en/open/standards/tiff/TIFFPM6.pdf - Tech Note 1: TIFF Trees
-/// IFD Leaf Fields: 
-var IfdSubdir = new byte[12];
+var SubIfdHeaderOffset = ifdValue;
 
-for (int i = 0; i < 16; ++i)
+Console.WriteLine("SubIFD Header:\tOffset = 0x{0:X}", SubIfdHeaderOffset);
+
+Console.WriteLine("~~~");
+
+// -------------------------------------------------------------------------- //
+
+/// SubIFD Header:
+
+var SubIfdHeader = new byte[2];
+
+RafFile.Position = CfaRecordOffset + SubIfdHeaderOffset;
+RafFile.Read(SubIfdHeader);
+
+var SubIfdRecordCount = BinaryPrimitives.ReadInt16LittleEndian(SubIfdHeader[0..]);
+
+Console.WriteLine("SubIFD Header:\tCount = {0}", SubIfdRecordCount);
+
+Console.WriteLine("~~~");
+
+// -------------------------------------------------------------------------- //
+
+/// SubIFD Records:
+
+for (int i = 0; i < SubIfdRecordCount; ++i)
 {
-    file.Position = CfaRecordOffset + 0x1A + 2 + (12 * i);
-    file.Read(IfdSubdir);
+    var SubIfdRecord = new byte[12];
 
-    tag = BinaryPrimitives.ReadInt16LittleEndian(IfdSubdir[0..]);
-    type = BinaryPrimitives.ReadInt16LittleEndian(IfdSubdir[2..]);
-    count = BinaryPrimitives.ReadInt32LittleEndian(IfdSubdir[4..]);
-    value = BinaryPrimitives.ReadInt32LittleEndian(IfdSubdir[8..]);
+    RafFile.Position = CfaRecordOffset + SubIfdHeaderOffset + 2 + (i * 12);
+    RafFile.Read(SubIfdRecord);
 
-    Console.WriteLine("\nIFD Field #{2}:\tTag = {0:X};\tType = {1:X}", tag, type, i);
-    Console.WriteLine("IFD Field #{2}:\tCount = {0};\tValue = {1}", count, value, i);
+    var subIfdTag = BinaryPrimitives.ReadInt16LittleEndian(SubIfdRecord[0..]);
+    var subIfdType = BinaryPrimitives.ReadInt16LittleEndian(SubIfdRecord[2..]);
+    var subIfdCount = BinaryPrimitives.ReadInt32LittleEndian(SubIfdRecord[4..]);
+    var subIfdValue = BinaryPrimitives.ReadInt32LittleEndian(SubIfdRecord[8..]);
+
+    Console.WriteLine(
+        "SubIFD Field:\tTag = {0:X};\tType = {1};\tCount = {2};\tValue = {3}",
+        subIfdTag,
+        Enum.GetName(typeof(IfdType), subIfdType),
+        subIfdCount,
+        subIfdValue
+    );
 }
 
+Console.WriteLine("~~~");
 
+// -------------------------------------------------------------------------- //
 
-// CFA Data:
-var buffer = new byte[4992 * 3296 * 2];
+/// CFA Records:
 
-file.Position = CfaRecordOffset + 0x800;
-file.Read(buffer);
+var buffer = new byte[6384 * 4182 * 2];
 
-using (var image = new Image<L8>(4992, 3296))
+RafFile.Position = CfaRecordOffset + 0x800;
+RafFile.Read(buffer);
+
+using (var image = new Image<L8>(6384, 4182))
 {
     var offset = 0;
     for (int y = 0; y < image.Height; ++y)
@@ -145,8 +188,27 @@ using (var image = new Image<L8>(4992, 3296))
         for (int x = 0; x < image.Width; ++x)
         {
             offset = (y * image.Width + x) * 2;
-            image[x, y] = new L8((byte) (BinaryPrimitives.ReadInt16LittleEndian(buffer[offset..(offset + 2)]) / 64));
+            image[x, y] = new L8((byte)(BinaryPrimitives.ReadInt16LittleEndian(buffer[offset..(offset + 2)]) / 64));
         }
     }
     image.SaveAsPng("img/DSCF.png");
+}
+
+// -------------------------------------------------------------------------- //
+
+enum IfdType : uint
+{
+    Byte = 1,
+    Ascii = 2,
+    Short = 3,
+    Long = 4,
+    Ratio = 5,
+    SByte = 6,
+    Undefined = 7,
+    SShort = 8,
+    SLong = 9,
+    SRatio = 10,
+    Float = 11,
+    Double = 12,
+    Ifd = 13
 }
